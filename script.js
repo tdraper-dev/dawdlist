986f8aead40d4b9b7fe81506b85b0d47a08d1365
Travis Draper <tdanieldraper@gmail.com>
2021-03-17 14:20:43 -0400

    changes

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
 
+    var taskInput = $('.taskInput')
+    console.log('jQuery', taskInput);
+
+   taskInput.addEventListener('click', function(e) {
+       e.preventDefault();
+   })
+
+
+   var taskInput1 = document.getElementsByClassName('taskInput')[0];
+   console.log('getElements', taskInput1);
     
+    taskInput1.addEventListener('click', function(e) {
+        e.preventDefault();
+    })
+
+ 
 });

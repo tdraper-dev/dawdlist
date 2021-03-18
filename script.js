9a6be2a829bebdbd6513dc771b78c12b404613d7
Travis Draper <tdanieldraper@gmail.com>
2021-03-18 13:45:24 -0400

    stlylings and html changes

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 $(document).ready(function() {
 
-    var taskInput = $('.taskInput')
-    console.log('jQuery', taskInput);
-
-   taskInput.addEventListener('click', function(e) {
-       e.preventDefault();
-   })
-
-
-   var taskInput1 = document.getElementsByClassName('taskInput')[0];
-   console.log('getElements', taskInput1);
-    
-    taskInput1.addEventListener('click', function(e) {
-        e.preventDefault();
-    })
 
  
 });
